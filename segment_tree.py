b81930931ce546db3cc9fd87875dac56bde28914
ruis <sako.soya.01@gmail.com>
2023-07-14 10:50:49 +0900

    Addition of update function, minor changes

--- segment_tree.py
+++ segment_tree.py
@@ -7,34 +7,38 @@ class Segment_Tree:
         self.function = tree_type
         self.a = [0]*list_len + default_list
         for i in range(list_len-1, 0, -1):
-            self.a[i] = self.function(self.a[i*2], self.a[i*2+1])
+            self.a[i] = self.function(self.a[i<<1], self.a[(i<<1)+1])
         
+    def set(self, ind, val):
+        ind += self.N
+        self.a[ind] = val
+        while ind > 1:
+            self.a[ind>>1] = self.function(self.a[ind], self.a[ind^1])
+            ind >>= 1
+        
+    def update(self, ind, val):
+        ind += self.N
+        self.a[ind] = self.function(self.a[ind], val)
+        while ind > 1:
+            self.a[ind>>1] = self.function(self.a[ind], self.a[ind^1])
+            ind >>= 1
 
-    def set_element(self, ind, val):
-        index = ind + self.N
-        self.a[index] = val
-        while index > 1:
-            self.a[index//2] = self.function(self.a[index], self.a[index^1])
-            index >>= 1
-
-
-    def get_element(self, lb : int, rb : int):
+    def get(self, lb : int, rb : int):
         lb += self.N
         rb += self.N
         ans = self.element
-
         while lb < rb:
             if lb & 1:
                 ans = self.function(ans, self.a[lb])
                 lb += 1
             if rb & 1:
                 ans = self.function(ans, self.a[rb-1])
-                rb -= 1
             lb >>= 1
             rb >>= 1
-        
         return ans
-
     
-    def get(self, ind):
+    def get_val(self, ind):
         return self.a[ind+self.N]
+    
+    def get_list(self):
+        return self.a[self.N:]
